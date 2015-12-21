f58e9042ee2a0b1b24807d30c5ca028c9c334149
Dan Flynn <danfbflynn@gmail.com>
2015-12-21 09:31:33 -0500

    Experimental design notes

---
+++ chillexpdesign.R
@@ -0,0 +1,99 @@
+# Calculating chilling portions for new chilling experiment
+
+# 4 chilling levels, including control
+# 6 species
+# 12 reps / species / level
+# 2 time points after control
+3 * 6* 12 * 2
+6 * 12 
+
+chilltreat = c(1.5, 2.5, 5)
+
+timetreat = c(20, 35)
+
+spp = c("acesac", "acerub", "querub","hamvir","vibcas","franig")
+
+chillev = length(chilltreat)
+spno = length(spp)
+repno = 12
+timeno = length(timetreat)
+lengthout = chillev*spno*repno*timeno 
+
+time <- gl(timeno, chillev*spno*repno, length = lengthout, labels = timetreat) # two time blocks
+sp <- gl(spno, repno*chillev, length = lengthout, labels = spp) # within time block, have species repeat rep * chilllev times
+chill <- gl(chillev, repno, length = lengthout, labels = chilltreat) # for each species in each time block, repeat rep number of times
+reps <- gl(repno, 1, length = lengthout) # for each treatment combination, give number of reps
+
+
+design <- data.frame(time, sp, chill, rep=reps)
+
+design[1:50,]
+
+# Now calculating chilling portions
+
+library(chillR)
+
+# chill unit calcs, for each treatment
+
+chillcalc <- function(days, temp){
+	Year = 2016
+	JDay = as.numeric(gl(days, 24))
+	Hour = as.numeric(gl(24, 1, length = days*24))
+	Temp = temp
+	xx <- data.frame(Year, JDay, Hour, Temp)
+	chilling(xx, 1, days)
+	}
+
+chillportions <- vector()
+numdays <- vector()
+
+chilltreat = seq(1, 10, by = 0.5)
+timetreat = seq(10, 40, by = 5)
+
+for(i in chilltreat){
+	for(j in timetreat){
+	
+	chillportions <- c(chillportions, as.numeric(chillcalc(j, i)[6]))
+	numdays <- c(numdays, j)
+	}
+}
+	
+dat <- data.frame(days = numdays, 
+					chill = gl(length(chilltreat), length(timetreat), labels = chilltreat), 
+					cp = chillportions)
+
+library(ggplot2)
+
+#qplot(days, cp, color = chill, data = dat)
+
+qplot(chill, cp, color = days, data = dat, 
+	ylab = "Chill Portions \n (Dynamic Model)", 
+	xlab = "Chilling Treatment (°C)")
+
+### Experimental design
+
+chillportions <- vector()
+numdays <- vector()
+
+chilltreat = c(1, 4, 8)
+timetreat = c(15, 30)
+
+for(i in chilltreat){
+	for(j in timetreat){
+	
+	chillportions <- c(chillportions, as.numeric(chillcalc(j, i)[6]))
+	numdays <- c(numdays, j)
+	}
+}
+	
+dat <- data.frame(days = numdays, 
+					chill = gl(length(chilltreat), length(timetreat), labels = chilltreat), 
+					cp = chillportions)
+
+library(ggplot2)
+
+#qplot(days, cp, color = chill, data = dat)
+
+qplot(chill, cp, color = days, data = dat, 
+	ylab = "Chill Portions \n (Dynamic Model)", 
+	xlab = "Chilling Treatment (°C)")
